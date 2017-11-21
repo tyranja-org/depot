11a3a91e0f51a533666ded3f748235e2a6bb09bf
Tyranja <tyranja@cassiopeia.uberspace.de>
2017-11-21 15:38:42 +0100

    Add line items

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
+    @session_count = increment_count
     @products = Product.order(:title)
+  end
+
+  private
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :title, length: { minimum: 10 }
@@ -7,4 +11,12 @@ class Product < ApplicationRecord
     with:   %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for a GIF, JPG or PNG image.'
   }
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
