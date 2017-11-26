e8cad8f769f1e48e97fe9460d9c6e68fe2cc8736
Tyranja <tyranja@cassiopeia.uberspace.de>
2017-11-26 17:50:54 +0100

    Adds tests

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,10 +3,13 @@ class Cart < ApplicationRecord
 
   def add_product(product)
     current_item = line_items.find_by(product_id: product.id)
+    current_price = Product.find(product.id).price if current_price.nil?
+
     if current_item
       current_item.quantity += 1
+      current_item.price = current_price
     else
-      current_item = line_items.build(product_id: product.id)
+      current_item = line_items.build(product_id: product.id, price: current_price)
     end
     current_item
   end
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "add unique products to a cart" do
+    cart = Cart.create
+    cart.add_product(products(:ruby)).save!
+    cart.add_product(products(:coffee)).save!
+
+    assert_equal 2, cart.line_items.count
+  end
+
+  test "add same products to a cart" do
+    cart = Cart.create
+    cart.add_product(products(:ruby)).save!
+    cart.add_product(products(:ruby)).save!
+
+    assert_equal 1, cart.line_items.count
+  end
+
 end
